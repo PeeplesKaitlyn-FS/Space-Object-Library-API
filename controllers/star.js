cb3aff8ea531e37dfcbe337b7c1bc56e7b6234db
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-22 21:51:26 -0500

    Update star.js

--- controllers/star.js
+++ controllers/star.js
@@ -1,32 +1,32 @@
-// Show all resources
-const index = (req, res) => {
-  // Respond with an array and 2xx status code
-  res.status(200).json([`Star#index`])
+const db = require('../models');
+const { Star } = db;
+module.exports = {
+  index: async (req, res) => {
+    const stars = await Star.findAll();
+    res.json(stars);
+  },
+  create: async (req, res) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const star = await Star.create({ name, description });
+    res.json(star);
+  },
+  show: async (req, res) => {
+    const id = req.params.id;
+    const star = await Star.findByPk(id);
+    res.json(star);
+  },
+  update: async (req, res) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const description = req.body.description;
+    await Star.update({ name, description }, { where: { id } });
+    const star = await Star.findByPk(id);
+    res.json(star);
+  },
+  remove: async (req, res) => {
+    const id = req.params.id;
+    await Star.destroy({ where: { id } });
+    res.json({ message: 'Star deleted successfully' });
+  }
 }
-
-// Show resource
-const show = (req, res) => {
-  // Respond with a single object and 2xx code
-  res.status(200).json(`Star#show(:id)`)
-}
-
-// Create a new resource
-const create = (req, res) => {
-  // Issue a redirect with a success 2xx code
-  res.redirect(`/stars`, 201)
-}
-
-// Update an existing resource
-const update = (req, res) => {
-  // Respond with a single resource and 2xx code
-  res.status(200).json(`/stars/${req.params.id}`, )
-}
-
-// Remove a single resource
-const remove = (req, res) => {
-  // Respond with a 2xx status code and bool
-  res.status(204).json(true)
-}
-
-// Export all controller actions
-module.exports = { index, show, create, update, remove }

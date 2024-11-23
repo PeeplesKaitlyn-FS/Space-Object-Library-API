cca816b41b399b679c698c159aa33832ed6fa409
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-22 23:13:05 -0500

    Update star.js

--- controllers/star.js
+++ controllers/star.js
@@ -1,32 +1,41 @@
-const db = require('../models');
-const { Star } = db;
-module.exports = {
-  index: async (req, res) => {
-    const stars = await Star.findAll();
-    res.json(stars);
-  },
-  create: async (req, res) => {
-    const name = req.body.name;
-    const description = req.body.description;
-    const star = await Star.create({ name, description });
-    res.json(star);
-  },
-  show: async (req, res) => {
-    const id = req.params.id;
-    const star = await Star.findByPk(id);
-    res.json(star);
-  },
-  update: async (req, res) => {
-    const id = req.params.id;
-    const name = req.body.name;
-    const description = req.body.description;
-    await Star.update({ name, description }, { where: { id } });
-    const star = await Star.findByPk(id);
-    res.json(star);
-  },
-  remove: async (req, res) => {
-    const id = req.params.id;
-    await Star.destroy({ where: { id } });
-    res.json({ message: 'Star deleted successfully' });
-  }
-}
+const express = require(`express`)
+const { DataTypes } = require('sequelize');
+const Star = require(`../models/Star`)(sequelize, DataTypes);
+const router = express.Router()
+
+
+
+router.get(`/`, async (req, res) => {
+  const stars = await Star.findAll()
+  res.json(stars)
+})
+
+router.post(`/`, async (req, res) => {
+  const name = req.body.name
+  const description = req.body.description
+  const star = await Star.create({ name, description })
+  res.json(star)
+})
+
+router.get(`/:id`, async (req, res) => {
+  const id = req.params.id
+  const star = await Star.findByPk(id)
+  res.json(star)
+})
+
+router.put(`/:id`, async (req, res) => {
+  const id = req.params.id
+  const star = await Star.findByPk(id)
+  star.name = req.body.name
+  star.description = req.body.description
+  await star.save()
+  res.json(star)
+})
+
+router.delete(`/:id`, async (req, res) => {
+  const id = req.params.id
+  await Star.destroy({ where: { id: id } })
+  res.json({ message: "Star deleted successfully" })
+})
+
+module.exports = router

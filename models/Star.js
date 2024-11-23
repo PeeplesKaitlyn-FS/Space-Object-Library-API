f8d01199ae379e859e105298ddef09ff3963dbef
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-22 21:51:38 -0500

    Create Star.js

---
+++ models/Star.js
@@ -0,0 +1,24 @@
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Star extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Star.belongsTo(models.Galaxy, { foreignKey: 'galaxyId' });
+      Star.hasMany(models.Planet, { foreignKey: 'starId' });
+    }
+  }
+  Star.init({
+    name: DataTypes.STRING,
+    size: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+    galaxyId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Star',
+  });
+  return Star;
+};

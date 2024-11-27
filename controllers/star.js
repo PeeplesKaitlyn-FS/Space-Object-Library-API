9aa36421f80e225aa860f2fed2533ac606d58681
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-27 03:16:19 -0500

    Update star.js

--- controllers/star.js
+++ controllers/star.js
@@ -4,21 +4,26 @@ const Star = db.Star;
 
 const starCtlr = {
   index: async (req, res) => {
+    const products = await Star.findAll();
+    res.render('views/products/index.twig', { products });
     const stars = await Star.findAll();
     res.json(stars);
   },
   create: async (req, res) => {
+    res.render('views/products/index.twig');
     const name = req.body.name;
     const description = req.body.description;
     const star = await Star.create({ name, description });
     res.json(star);
   },
   show: async (req, res) => {
+    res.render('views/products/index.twig');
     const id = req.params.id;
     const star = await Star.findByPk(id);
     res.json(star);
   },
   update: async (req, res) => {
+    res.status(200).json('product#update');
     const id = req.params.id;
     const star = await Star.findByPk(id);
     star.name = req.body.name;
@@ -27,10 +32,13 @@ const starCtlr = {
     res.json(star);
   },
   remove: async (req, res) => {
+    res.status(200).json('product#remove');
     const id = req.params.id;
     await Star.destroy({ where: { id: id } });
     res.json({ message: "Star deleted successfully" });
+  },
+  form: async (req, res) => {
+    res.status(200).json('product#form(:id)');
   }
 };
-
 module.exports = starCtlr;

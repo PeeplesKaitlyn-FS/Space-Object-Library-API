7139f1a7551cd3a39ea4d956ba6fb88ccbe8fdf3
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-26 23:47:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,35 @@
 const express = require(`express`)
 // Create a new Express instance called "app"
 const app = express()
+
+const { Planet, Star, Galaxy } = require('./models');
 // Load in our JSON parsing middleware
 app.use(express.json());
 
 // Load in our RESTful routers
 const routers = require('./routers/index.js')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'twig');
 
 // Home page welcome middleware
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Welcome to Star Tracker Library')
+app.get('/', async (req, res) => {
+  const planets = await db.Planet.findByPk(3);
+  res.render('home', { planets: planets });
+});
+
+app.get('/planets', async (req, res) => {
+  const planets = await db.Planet.findAll();
+  res.render('planets', { planets: planets });
+}); 
+
+app.get('/stars', async (req, res) => {
+  const stars = await db.Star.findAll();
+  res.render('stars', { stars: stars });
+}); 
+
+app.get('/galaxies', async (req, res) => {
+  const galaxies = await db.Galaxy.findAll();
+  res.render('galaxies', { galaxies: galaxies });
 })
 
 // Register our RESTful routers with our "app"

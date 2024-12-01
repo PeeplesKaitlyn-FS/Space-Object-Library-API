ec5bb658fbb546250499d9957977570c49e18c3b
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-12-01 17:23:54 -0500

    Update galaxy.js

--- controllers/galaxy.js
+++ controllers/galaxy.js
@@ -3,19 +3,19 @@ const { Galaxy } = require(`../models`);
 // Show all resources
 const index = async (req, res) => {
   const products = await Galaxy.findAll();
-  res.render('views/products/index.twig', { products });
+  res.render('views/galaxies/index.twig', { products });
 }
-
 
 // Show resource
 const show = async (req, res) => {
-  res.render('views/products/index.twig');
+  res.render('views/galaxies/index.twig');
 }
-
 
 // Create a new resource
 const create = async (req, res) => {
-  res.render('views/products/index.twig');
+  const product = await Galaxy.create(req.body);
+  res.redirect(302, `/galaxies/${product.id}`);
+  res.render('views/galaxies/index.twig');
 }
 
 // Update an existing resource
@@ -23,12 +23,16 @@ const update = async (req, res) => {
   res.status(200).json('product#update')
 }
 
-
 // Remove a single resource
 const remove = async (req, res) => {
 }
-const form = (req, res) => {
-  res.status(200).json('product#form(:id)')
-}
+const form = async (req, res) => {
+    if ('undefined' !== typeof req.params.id) {
+      const product = await Galaxy.findByPk(req.params.id);
+      res.render('views/product/_form.twig', { product });
+    } else {
+      res.render('views/product/_form.twig');
+    }
+  }
 
 module.exports = { index, show, create, update, remove, form }

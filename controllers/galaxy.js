c17903687c8a43d9bb50b8bf73566e1b84453d34
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-12-01 17:54:25 -0500

    Update galaxy.js

--- controllers/galaxy.js
+++ controllers/galaxy.js
@@ -1,4 +1,14 @@
-const { Galaxy } = require(`../models`);
+const { Sequelize } = require('../db');
+const db = require('../db');
+
+const Galaxy = db.define(`galaxy`, {
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  }
+});
 
 // Show all resources
 const index = async (req, res) => {
@@ -8,31 +18,41 @@ const index = async (req, res) => {
 
 // Show resource
 const show = async (req, res) => {
-  res.render('views/galaxies/index.twig');
+  const id = req.params.id;
+  const product = await Galaxy.findByPk(id);
+  res.render('views/galaxies/index.twig', { product });
 }
 
 // Create a new resource
 const create = async (req, res) => {
   const product = await Galaxy.create(req.body);
   res.redirect(302, `/galaxies/${product.id}`);
-  res.render('views/galaxies/index.twig');
 }
 
 // Update an existing resource
 const update = async (req, res) => {
-  res.status(200).json('product#update')
+  const id = req.params.id;
+  const product = await Galaxy.findByPk(id);
+  product.name = req.body.name;
+  product.description = req.body.description;
+  await product.save();
+  res.redirect(302, `/galaxies/${product.id}`);
 }
 
 // Remove a single resource
 const remove = async (req, res) => {
+  const id = req.params.id;
+  await Galaxy.destroy({ where: { id } });
+  res.redirect(302, `/galaxies`);
 }
+
 const form = async (req, res) => {
-    if ('undefined' !== typeof req.params.id) {
-      const product = await Galaxy.findByPk(req.params.id);
-      res.render('views/product/_form.twig', { product });
-    } else {
-      res.render('views/product/_form.twig');
-    }
+  if ('undefined' !== typeof req.params.id) {
+    const product = await Galaxy.findByPk(req.params.id);
+    res.render('views/product/_form.twig', { product });
+  } else {
+    res.render('views/product/_form.twig');
   }
+}
 
 module.exports = { index, show, create, update, remove, form }

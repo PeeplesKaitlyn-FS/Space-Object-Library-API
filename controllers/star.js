18c09a8221416cee9e678002e373443108baecf2
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-12-01 17:54:28 -0500

    Update star.js

--- controllers/star.js
+++ controllers/star.js
@@ -1,4 +1,14 @@
-const { Star } = require('../models');
+const { Sequelize } = require('../db');
+const db = require('../db');
+
+const Star = db.define(`star`, {
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  }
+});
 
 const starCtlr = {
   index: async (req, res) => {
@@ -6,20 +16,17 @@ const starCtlr = {
     res.render('./stars/index.twig', { products });
   },
   create: async (req, res) => {
-    res.render('views/stars/index.twig');
     const name = req.body.name;
     const description = req.body.description;
     const star = await Star.create({ name, description });
     res.json(star);
   },
   show: async (req, res) => {
-    res.render('views/stars/index.twig');
     const id = req.params.id;
     const star = await Star.findByPk(id);
     res.json(star);
   },
   update: async (req, res) => {
-    res.status(200).json('product#update');
     const id = req.params.id;
     const star = await Star.findByPk(id);
     star.name = req.body.name;
@@ -28,9 +35,8 @@ const starCtlr = {
     res.json(star);
   },
   remove: async (req, res) => {
-    res.status(200).json('product#remove');
     const id = req.params.id;
-    await Star.destroy({ where: { id: id } });
+    await Star.destroy({ where: { id } });
     res.json({ message: "Star deleted successfully" });
   },
   form: async (req, res) => {
@@ -42,4 +48,5 @@ const starCtlr = {
     }
   }
 };
+
 module.exports = starCtlr;

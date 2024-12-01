9c931dd849197893267af25d398a1a45f9939c2e
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-12-01 17:23:51 -0500

    Update galaxy.js

--- routers/galaxy.js
+++ routers/galaxy.js
@@ -10,15 +10,15 @@ const router = express.Router()
 // RESTful resource mappings
 router.get(`/`, galaxyCtlr.index)
 router.post(`/`, galaxyCtlr.create)
-router.get(`/:id`, galaxyCtlr.show) 
-router.put(`/:id`, galaxyCtlr.update) 
-router.delete(`/:id`, galaxyCtlr.remove) 
+router.get(`/:id(\d+)`, galaxyCtlr.show) 
+router.put(`/:id(\d+)`, galaxyCtlr.update) 
+router.delete(`/:id(\d+)`, galaxyCtlr.remove) 
 
 //html5 specific routes
 router.get(`/new`, galaxyCtlr.form)
-router.get(`/:id/edit`, galaxyCtlr.form)
-router.get(`/:id/delete`, galaxyCtlr.remove)
-router.post(`/:id/`, galaxyCtlr.update)
+router.get(`/:id(\d+)/edit`, galaxyCtlr.form)
+router.get(`/:id(\d+)/delete`, galaxyCtlr.remove)
+router.post(`/:id(\d+)/`, galaxyCtlr.update)
 
 // export "router"
 module.exports = router

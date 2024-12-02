a472532170573292cec58c242568edfb467a938e
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-12-01 19:55:42 -0500

    Update galaxy.js

--- controllers/galaxy.js
+++ controllers/galaxy.js
@@ -5,6 +5,9 @@ const Galaxy = db.define(`galaxy`, {
   name: {
     type: Sequelize.STRING
   },
+  size: {
+    type: Sequelize.STRING
+  },
   description: {
     type: Sequelize.STRING
   }
@@ -12,31 +15,32 @@ const Galaxy = db.define(`galaxy`, {
 
 // Show all resources
 const index = async (req, res) => {
-  const products = await Galaxy.findAll();
-  res.render('views/galaxies/index.twig', { products });
+  const galaxies = await Galaxy.findAll();
+  res.render('views/galaxies/index.twig', { galaxies });
 }
 
 // Show resource
 const show = async (req, res) => {
   const id = req.params.id;
-  const product = await Galaxy.findByPk(id);
-  res.render('views/galaxies/index.twig', { product });
+  const galaxy = await Galaxy.findByPk(id);
+  res.render('views/galaxies/show.twig', { galaxy });
 }
 
 // Create a new resource
 const create = async (req, res) => {
-  const product = await Galaxy.create(req.body);
-  res.redirect(302, `/galaxies/${product.id}`);
+  const galaxy = await Galaxy.create(req.body);
+  res.redirect(302, `/galaxies/${galaxy.id}`);
 }
 
 // Update an existing resource
 const update = async (req, res) => {
   const id = req.params.id;
-  const product = await Galaxy.findByPk(id);
-  product.name = req.body.name;
-  product.description = req.body.description;
-  await product.save();
-  res.redirect(302, `/galaxies/${product.id}`);
+  const galaxy = await Galaxy.findByPk(id);
+  galaxy.name = req.body.name;
+  galaxy.size = req.body.size;
+  galaxy.description = req.body.description;
+  await galaxy.save();
+  res.redirect(302, `/galaxies/${galaxy.id}`);
 }
 
 // Remove a single resource
@@ -48,8 +52,8 @@ const remove = async (req, res) => {
 
 const form = async (req, res) => {
   if ('undefined' !== typeof req.params.id) {
-    const product = await Galaxy.findByPk(req.params.id);
-    res.render('views/product/_form.twig', { product });
+    const galaxy = await Galaxy.findByPk(req.params.id);
+    res.render('views/product/_form.twig', { galaxy });
   } else {
     res.render('views/product/_form.twig');
   }
